9e87017ba06e71c704633139b22d706a2be777cb
Soonyub <soonyub.hwang@jp.12thwonder.com>
2017-12-15 00:10:40 +0900

    ORM System initialize

---
+++ PTM.ORM/Entity/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.OleDb;
+using PTM.ORM.Attribute;
+using PTM.ORM.Common;
+
+namespace PTM.ORM.Entity
+{
+    [Table("Test")]
+    public sealed class Test : IEntity
+    {
+        [Column("idx", OleDbType.Integer, Key = true, Identity = true)]
+        private int idx;
+
+        public int Idx
+        {
+            get { return this.idx; }
+        }
+
+        [Column("data", OleDbType.VarChar)]
+        private String data;
+
+        public String Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+    }
+}
---
+++ PTM.ORM/ORMFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ADOX;
+using System.IO;
+using PTM.ORM.Impl;
+using PTM.ORM.Common;
+using PTM.ORM.Dao;
+
+namespace PTM.ORM
+{
+    public class ORMFactory
+    {
+        private static ORMFactory instance = null;
+        public static T GetService<T>(Type type) where T : IDao
+        {
+            if (instance == null)
+            {
+                instance = new ORMFactory();
+            }
+            if (!instance.servicemap.ContainsKey(type))
+            {
+                throw new Exception("The instance dao is not declared.");
+            }
+            return (T)instance.servicemap[type];
+        }
+
+        private Dictionary<Type, IDao> servicemap;
+        private ORMFactory()
+        {
+            Database db = new Database();
+            servicemap = new Dictionary<Type, IDao>();
+            // DI Bindings
+            servicemap.Add(typeof(ITestDao), new TestDao(db));
+        }
+    }
+}
